81faa71ddd86012740b7f8f91261860a8d6e99e9
James Raines <jraines521@gmail.com>
2018-11-20 09:11:25 -0600

    Update HIFLD_download_geoprocessing.py

--- HIFLD_download_geoprocessing.py
+++ HIFLD_download_geoprocessing.py
@@ -6,7 +6,7 @@ Created on Fri Sep 21 12:20:02 2018
 
 Automated HIFLD data download. 
 Requires downloading the HIFLD data catalog and renaming the REST API endpoint field to rest_endpoint and save as CSV.
-Some ancillary fields interfer with geopandas ability to open the CSV and need to be deleted.
+Some ancillary fields interfere with geopandas ability to open the CSV and need to be deleted.
 """ 
 
 import geopandas as gpd
@@ -20,7 +20,7 @@ from time import time
  
 def getGeoJson(rest_endpoint):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-#    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
     payload = {'f': 'geojson', 'outSR': '4326', 'outFields': '*', 'where': '1%3D1'}
     baseURL = [rest_endpoint]
     baseURL.append('/query?')
@@ -58,7 +58,7 @@ def getData(dataCatalog, outputDir, startIndex, endIndex):
     for i in range(len(dataCatalog)):
         if i > startIndex and i < endIndex:
             geoJson, status_code, name = getGeoJson(dataCatalog.iloc[i]['rest_endpoint'])
-            if status_code == 200:
+            if (status_code == 200) and (list(geoJson.keys())[0] != 'error'):
                 with open('temporary_598c83h34e9e098e72s9e.json', 'w') as gj:
                     json.dump(geoJson, gj)
                 success = geojsonToShapefile('temporary_598c83h34e9e098e72s9e.json', outputDir, name + '.shp')
@@ -89,7 +89,8 @@ def bufferIntersect(directory, buffer_size):
     shp1['const'] = 1
     shp2['const'] = 1
     intersect = shp1.dissolve('const').intersection(shp2.dissolve('const'))
-    intersect.to_file(filename= 'intersect.shp', driver='ESRI Shapefile', crs_wkt=source_crs_wkt)
+    gdf = GeoSeriesToGeoDataFrame(intersect, 'MultiPolygon')
+    gdf.to_file(filename= 'intersect.shp', driver='ESRI Shapefile', crs_wkt=source_crs_wkt)
     for index in range(len(buffers)):
         if (index + 2) < len(buffers):
             print(index + 2)
@@ -98,25 +99,23 @@ def bufferIntersect(directory, buffer_size):
             shp1['const'] = 1
             shp2['const'] = 1
             intersect = shp1.dissolve('const').intersection(shp2.dissolve('const'))
-            intersect.to_file(filename= 'intersect.shp', driver='ESRI Shapefile', crs_wkt=source_crs_wkt)
+            gdf = GeoSeriesToGeoDataFrame(intersect, 'MultiPolygon')
+            gdf.to_file(filename= 'intersect.shp', driver='ESRI Shapefile', crs_wkt=source_crs_wkt)
             
+# in case the GeoSeries needs to be converted to a GeoDataFrame
+def GeoSeriesToGeoDataFrame(geoseries, geom_type):
+    gdf = gpd.GeoDataFrame()
+    gdf['geometry'] = geoseries[geoseries.geom_type == geom_type]
+    gdf = gdf.rename(columns={0:'geometry'}).set_geometry('geometry')
+    gdf.plot(figsize=(5,5))
+    return gdf
+
 
 # Set Variables
 os.chdir('C:/test/')
 dataCatalog = pd.read_csv('HIFLD_2018_Data_Catalog.csv')
-outputDir = 'HIFLD_requests'
+outputDir = 'HIFLD_requests2'
 
 # Run Functions
-getData(dataCatalog, outputDir, 3, 8)
-bufferIntersect('C:/test/HIFLD_requests/', 0.6)
-
-
-"""
-# in case the GeoSeries needs to be converted to a GeoDataFrame
-
-gdf = gpd.GeoDataFrame()
-gdf['geometry'] = intersect[intersect.geom_type == 'MultiPolygon']
-gdf = gdf.rename(columns={0:'geometry'}).set_geometry('geometry')
-gdf.plot(figsize=(15,15))
-gdf.to_file(filename='intersect.shp', driver='ESRI Shapefile', crs_wkt=source_crs_wkt)
-"""
+getData(dataCatalog, outputDir, 23, 30)
+bufferIntersect('C:/test/HIFLD_requests2/', 0.6)
